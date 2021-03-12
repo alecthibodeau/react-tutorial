af31d315db28292ea3bae8d53981c51f8d6a1f9c
alecthibodeau <alecthibodeau@gmail.com>
2021-03-12 13:51:38 -0500

    Add helper function to calculate winner. Details: - Add helper function to determine winner or null. - Disable gameplay on selected squares or if there's a winner.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ class Board extends React.Component {
   // The state is stored in the Board component instead of the individual Square components
   handleClick (i) {
     const squares = this.state.squares.slice();
+    // Return early by ignoring a click if someone has won the game or if a Square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -92,3 +96,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
